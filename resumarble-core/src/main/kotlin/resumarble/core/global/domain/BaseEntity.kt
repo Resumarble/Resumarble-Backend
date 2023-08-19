1682224c695562cd46c125835f26e8ed24cd38f0
waveofmymind <sjun0913@gmail.com>
2023-08-20 02:47:00 +0900

    feat(core): 생성일과 수정일에 대한 추상 클래스를 생성한다.

---
+++ resumarble-core/src/main/kotlin/resumarble/core/global/domain/BaseEntity.kt
@@ -0,0 +1,15 @@
+package resumarble.core.global.domain
+
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import java.time.LocalDateTime
+
+@MappedSuperclass
+abstract class BaseEntity {
+    @CreatedDate
+    private val createdDate: LocalDateTime = LocalDateTime.now()
+
+    @LastModifiedDate
+    private val lastModifiedDate: LocalDateTime = LocalDateTime.now()
+}
