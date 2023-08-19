61facfc51e4f98095e1e24644001221524c3cac2
waveofmymind <sjun0913@gmail.com>
2023-08-20 02:55:42 +0900

    refactor(job): wildcard import를 사용하지 않는다.

--- resumarble-core/src/main/kotlin/resumarble/core/domain/job/domain/Job.kt
+++ resumarble-core/src/main/kotlin/resumarble/core/domain/job/domain/Job.kt
@@ -1,6 +1,11 @@
 package resumarble.core.domain.job.domain
 
-import jakarta.persistence.*
+import jakarta.persistence.Embedded
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
 import resumarble.core.global.domain.BaseEntity
 
 @Entity
