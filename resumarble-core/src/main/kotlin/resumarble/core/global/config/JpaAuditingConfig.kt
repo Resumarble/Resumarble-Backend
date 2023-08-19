ac381b126619383152f80118705df9c6143b0f3f
waveofmymind <sjun0913@gmail.com>
2023-08-20 03:01:38 +0900

    feat(core): 생성일, 수정일에 대한 Auditing 클래스를 추가한다.

---
+++ resumarble-core/src/main/kotlin/resumarble/core/global/config/JpaAuditingConfig.kt
@@ -0,0 +1,8 @@
+package resumarble.core.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class JpaAuditingConfig
