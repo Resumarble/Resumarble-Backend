270f7dedc7b146dfaee5f4a3b7832490921adffc
waveofmymind <sjun0913@gmail.com>
2023-08-19 23:50:25 +0900

    feat(api): api모듈은 core 모듈을 의존한다.

--- resumarble-api/src/main/kotlin/resumarble/Application.kt
+++ resumarble-api/src/main/kotlin/resumarble/Application.kt
@@ -3,7 +3,8 @@ package resumarble
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(
+    scanBasePackages = ["resumarble.core", "resumarble.api"])
 class Application
 
 fun main(args: Array<String>) {
