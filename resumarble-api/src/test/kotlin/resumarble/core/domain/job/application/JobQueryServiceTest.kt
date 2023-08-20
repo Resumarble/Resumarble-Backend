b0fcc0ca096a037cecd079ea2494712af8a7cb51
waveofmymind <sjun0913@gmail.com>
2023-08-20 10:02:11 +0900

    test(job): 조회 목록 테스트를 구현한다.

---
+++ resumarble-api/src/test/kotlin/resumarble/core/domain/job/application/JobQueryServiceTest.kt
@@ -0,0 +1,28 @@
+package resumarble.core.domain.job.application
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import resumarble.core.domain.application.JobQueryService
+import resumarble.core.domain.job.JobFixture
+import resumarble.core.domain.job.infrastructure.JobRepository
+
+class JobQueryServiceTest : BehaviorSpec({
+
+    val jobRepository = mockk<JobRepository>()
+    val sut = JobQueryService(jobRepository)
+
+    Given("직업 목록을 조회하는 경우") {
+        val jobs = JobFixture.jobs()
+        When("정상적인 요청일 때") {
+            every { jobRepository.findAll() } returns jobs
+            Then("직업 목록이 반환된다.") {
+                sut.getAllJobs()
+                verify(exactly = 1) {
+                    jobRepository.findAll()
+                }
+            }
+        }
+    }
+})
