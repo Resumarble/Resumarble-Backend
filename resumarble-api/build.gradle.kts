d62867cbebb55d1f43641c56c0f3331625331b34
waveofmymind <sjun0913@gmail.com>
2023-08-21 11:30:06 +0900

    feat(api): 설정 파일에 대한 암호화를 위해 Jasypt 설정을 추가한다.

--- resumarble-api/build.gradle.kts
+++ resumarble-api/build.gradle.kts
@@ -1,16 +1,31 @@
-tasks.getByName("bootJar") {
+tasks.getByName<Jar>("bootJar") {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
     enabled = true
 }
 
-tasks.getByName("jar") {
+tasks.getByName<Jar>("jar") {
     enabled = false
 }
+
+extra["springCloudVersion"] = "2022.0.4"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
 
+    // spring cloud config
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
+
     // swagger-ui
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
 
+    // jasypt
+    implementation("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.5")
+
     api(project(":resumarble-core"))
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
 }
