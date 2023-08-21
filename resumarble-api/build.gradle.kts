68fc7eaf518a7bc1d10b2489a3bbd0382b49ed3b
waveofmymind <sjun0913@gmail.com>
2023-08-21 13:19:13 +0900

    feat(root): AI 서비스를 사용하기 위해, Open Feign 의존성을 추가한다.

--- resumarble-api/build.gradle.kts
+++ resumarble-api/build.gradle.kts
@@ -12,8 +12,12 @@ extra["springCloudVersion"] = "2022.0.4"
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
 
+    // spring cloud open feign
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+
     // spring cloud config
     implementation("org.springframework.cloud:spring-cloud-starter-config")
+    implementation("org.springframework.cloud:spring-cloud-starter-bus-kafka")
 
     // swagger-ui
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
