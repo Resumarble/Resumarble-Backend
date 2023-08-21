d5044b5996d8d010ccf1e0fd25b577e3d933ea02
waveofmymind <sjun0913@gmail.com>
2023-08-21 14:19:39 +0900

    fix(root): open feign 의존성은 코어 모듈에만 국한되어 사용된다.

--- resumarble-api/build.gradle.kts
+++ resumarble-api/build.gradle.kts
@@ -12,9 +12,6 @@ extra["springCloudVersion"] = "2022.0.4"
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
 
-    // spring cloud open feign
-    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
-
     // spring cloud config
     implementation("org.springframework.cloud:spring-cloud-starter-config")
     implementation("org.springframework.cloud:spring-cloud-starter-bus-kafka")
--- resumarble-core/build.gradle.kts
+++ resumarble-core/build.gradle.kts
@@ -6,9 +6,20 @@ tasks.getByName("jar") {
     enabled = true
 }
 
+extra["springCloudVersion"] = "2022.0.4"
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
+    // spring cloud open feign
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+
     implementation("org.springframework:spring-webmvc:6.0.11")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
 }
