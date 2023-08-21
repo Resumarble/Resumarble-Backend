7c2444ae105b5e3cdf31743dc41db74e7c9549a5
waveofmymind <sjun0913@gmail.com>
2023-08-21 18:05:16 +0900

    refactor(core): 로그 설정을 추가하고, const를 private으로 변경한다.

--- resumarble-core/src/main/kotlin/resumarble/core/global/config/FeignConfig.kt
+++ resumarble-core/src/main/kotlin/resumarble/core/global/config/FeignConfig.kt
@@ -1,5 +1,6 @@
 package resumarble.core.global.config
 
+import feign.Logger
 import feign.Retryer
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.cloud.openfeign.FeignClientsConfiguration
@@ -8,9 +9,12 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 
 @Configuration
-@EnableFeignClients
+@EnableFeignClients("resumarble.core.domain.gpt.client")
 @Import(FeignClientsConfiguration::class)
 class FeignConfig {
+
+    @Bean
+    fun log() = Logger.Level.FULL
 
     @Bean
     fun retry() = Retryer.Default(
@@ -20,8 +24,8 @@ class FeignConfig {
     )
 
     companion object {
-        const val INITIAL_BACKOFF_PERIOD: Long = 1000 * 5
-        const val MAX_BACKOFF_PERIOD: Long = 1000 * 5
-        const val MAX_RETRY_ATTEMPTS = 3
+        private const val INITIAL_BACKOFF_PERIOD: Long = 1000 * 5
+        private const val MAX_BACKOFF_PERIOD: Long = 1000 * 5
+        private const val MAX_RETRY_ATTEMPTS = 3
     }
 }
