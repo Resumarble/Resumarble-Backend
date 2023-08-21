434e3e3302fabefb192479ee1d1125ac8ee3264d
waveofmymind <sjun0913@gmail.com>
2023-08-22 01:42:04 +0900

    style(gpt): 클래스 명을 명확히 변경한다.

--- resumarble-api/src/main/kotlin/resumarble/api/presentation/resume/ResumeController.kt
+++ resumarble-api/src/main/kotlin/resumarble/api/presentation/resume/ResumeController.kt
@@ -4,8 +4,8 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import resumarble.core.domain.gpt.ChatCompletionsMessage
-import resumarble.core.domain.gpt.ChatCompletionsRequest
+import resumarble.core.domain.gpt.ChatCompletionMessage
+import resumarble.core.domain.gpt.ChatCompletionRequest
 import resumarble.core.domain.gpt.OpenAiRole
 import resumarble.core.domain.gpt.application.OpenAiService
 
@@ -20,10 +20,10 @@ class ResumeController(
         @RequestBody request: InterviewQuestionRequest
     ): String {
         val response = openAiService.requestOpenAiChatCompletion(
-            ChatCompletionsRequest(
+            ChatCompletionRequest(
                 messages =
                 mutableListOf(
-                    ChatCompletionsMessage(
+                    ChatCompletionMessage(
                         role = OpenAiRole.SYSTEM.value,
                         content = "Hello? What is Your name?"
                     )
