a3fafb8c1f9de5f5da28abbf2516150a2117a74e
waveofmymind <sjun0913@gmail.com>
2023-08-23 13:47:06 +0900

    feat(resume): swagger 어노테이션을 추가한다.

--- resumarble-api/src/main/kotlin/resumarble/api/presentation/resume/ResumeController.kt
+++ resumarble-api/src/main/kotlin/resumarble/api/presentation/resume/ResumeController.kt
@@ -1,5 +1,7 @@
 package resumarble.api.presentation.resume
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -8,11 +10,17 @@ import resumarble.api.response.Response
 import resumarble.core.domain.resume.facade.InterviewQuestionResponse
 import resumarble.core.domain.resume.facade.ResumeFacade
 
+@Tag(name = "ResumeController", description = "면접 예상 질문 생성 API")
 @RestController
 @RequestMapping("/resumes")
 class ResumeController(
     private val resumeFacade: ResumeFacade
 ) {
+    @Operation(
+        operationId = "interview-questions",
+        summary = "면접 예상 질문 생성",
+        description = "이력서를 기반으로 면접 예상 질문을 생성한다."
+    )
     @PostMapping("/interview-questions")
     fun interviewQuestions(
         @RequestBody request: InterviewQuestionRequest
