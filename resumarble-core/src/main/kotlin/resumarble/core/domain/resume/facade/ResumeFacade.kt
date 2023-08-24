9947a36abdbd22c0353def64052461533577961f
waveofmymind <sjun0913@gmail.com>
2023-08-24 12:45:10 +0900

    feat(core): 트레일링 람다를 사용해서 로그 시간을 추적할 수 있다.

--- resumarble-core/src/main/kotlin/resumarble/core/domain/resume/facade/ResumeFacade.kt
+++ resumarble-core/src/main/kotlin/resumarble/core/domain/resume/facade/ResumeFacade.kt
@@ -7,6 +7,9 @@ import resumarble.core.domain.gpt.application.OpenAiService
 import resumarble.core.domain.prompt.application.PromptService
 import resumarble.core.domain.prompt.domain.PromptType
 import resumarble.core.global.annotation.Facade
+import resumarble.core.global.util.LoggerUtil.logger
+import java.time.Duration
+import java.time.LocalDateTime
 
 @Facade
 class ResumeFacade(
@@ -16,14 +19,38 @@ class ResumeFacade(
     private val objectMapper: ObjectMapper
 ) {
 
+    val logger = logger()
+
     fun generateInterviewQuestion(command: InterviewQuestionCommand): InterviewQuestionResponse {
-        val promptResponse = promptService.getPrompt(PromptType.INTERVIEW_QUESTION)
-        val requestForm =
-            promptResponse.createRequestForm(command.job, command.career, command.category, command.career, "korean")
-        val completionRequest =
-            openAiMapper.promptAndContentToChatCompletionRequest(requestForm, promptResponse.content)
-        return objectMapper.readValue<InterviewQuestionResponse>(
-            openAiService.requestChatCompletion(completionRequest).choices[0].message.content
-        )
+        return loggingStopWatch {
+            val promptResponse = promptService.getPrompt(PromptType.INTERVIEW_QUESTION)
+            val requestForm =
+                promptResponse.createRequestForm(
+                    command.job,
+                    command.career,
+                    command.category,
+                    command.career,
+                    "korean"
+                )
+            val completionRequest =
+                openAiMapper.promptAndContentToChatCompletionRequest(requestForm, promptResponse.content)
+            objectMapper.readValue<InterviewQuestionResponse>(
+                openAiService.requestChatCompletion(completionRequest).choices[0].message.content
+            )
+        }
+    }
+
+    fun <T> loggingStopWatch(function: () -> T): T {
+        val startAt = LocalDateTime.now()
+        logger.info("Start at $startAt")
+
+        val result = function.invoke()
+
+        val endAt = LocalDateTime.now()
+
+        logger.info("End at $endAt")
+        logger.info("Logic Duration : ${Duration.between(startAt, endAt).toMillis()}ms")
+
+        return result
     }
 }
