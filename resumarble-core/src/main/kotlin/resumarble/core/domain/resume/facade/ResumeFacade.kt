d80ff7e5225e2379a7822f6b53844bac869d148b
waveofmymind <sjun0913@gmail.com>
2023-08-25 15:03:16 +0900

    refactor(core): 책임을 분리해서 로직에 대한 가독성을 높인다.

--- resumarble-core/src/main/kotlin/resumarble/core/domain/resume/facade/ResumeFacade.kt
+++ resumarble-core/src/main/kotlin/resumarble/core/domain/resume/facade/ResumeFacade.kt
@@ -1,56 +1,34 @@
 package resumarble.core.domain.resume.facade
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
 import resumarble.core.domain.gpt.OpenAiMapper
 import resumarble.core.domain.gpt.application.OpenAiService
 import resumarble.core.domain.prompt.application.PromptService
 import resumarble.core.domain.prompt.domain.PromptType
 import resumarble.core.global.annotation.Facade
-import resumarble.core.global.util.LoggerUtil.logger
-import java.time.Duration
-import java.time.LocalDateTime
+import resumarble.core.global.util.loggingStopWatch
 
 @Facade
 class ResumeFacade(
     private val promptService: PromptService,
     private val openAiService: OpenAiService,
-    private val openAiMapper: OpenAiMapper,
-    private val objectMapper: ObjectMapper
+    private val openAiMapper: OpenAiMapper
 ) {
-
-    val logger = logger()
 
     fun generateInterviewQuestion(command: InterviewQuestionCommand): InterviewQuestionResponse {
         return loggingStopWatch {
             val promptResponse = promptService.getPrompt(PromptType.INTERVIEW_QUESTION)
-            val requestForm =
-                promptResponse.createRequestForm(
-                    command.job,
-                    command.career,
-                    command.category,
-                    command.career,
-                    "korean"
-                )
+            val completionRequestForm = command.toRequestForm(promptResponse, PROMPT_LANGUAGE)
+
             val completionRequest =
-                openAiMapper.promptAndContentToChatCompletionRequest(requestForm, promptResponse.content)
-            objectMapper.readValue<InterviewQuestionResponse>(
-                openAiService.requestChatCompletion(completionRequest).choices[0].message.content
+                openAiMapper.promptAndContentToChatCompletionRequest(completionRequestForm, promptResponse.content)
+
+            openAiMapper.completionToInterviewQuestionResponse(
+                openAiService.requestChatCompletion(completionRequest)
             )
         }
     }
 
-    fun <T> loggingStopWatch(function: () -> T): T {
-        val startAt = LocalDateTime.now()
-        logger.info("Start at $startAt")
-
-        val result = function.invoke()
-
-        val endAt = LocalDateTime.now()
-
-        logger.info("End at $endAt")
-        logger.info("Logic Duration : ${Duration.between(startAt, endAt).toMillis()}ms")
-
-        return result
+    companion object {
+        private const val PROMPT_LANGUAGE = "korean"
     }
 }
