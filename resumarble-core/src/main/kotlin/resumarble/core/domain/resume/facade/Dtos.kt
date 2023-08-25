bd6d4e4f3795aacbc212383dbfeb08fddad2f167
waveofmymind <sjun0913@gmail.com>
2023-08-25 15:01:47 +0900

    refactor(core): getter를 사용하지 않고, 프롬프트에게 메시지를 보낸다

--- resumarble-core/src/main/kotlin/resumarble/core/domain/resume/facade/Dtos.kt
+++ resumarble-core/src/main/kotlin/resumarble/core/domain/resume/facade/Dtos.kt
@@ -1,11 +1,17 @@
 package resumarble.core.domain.resume.facade
 
+import resumarble.core.domain.prompt.application.PromptResponse
+
 data class InterviewQuestionCommand(
     val job: String,
     val career: String,
     val category: String,
     val content: String
-)
+) {
+    fun toRequestForm(prompt: PromptResponse, language: String): String {
+        return prompt.createRequestForm(job, career, category, content, language)
+    }
+}
 
 data class InterviewQuestionResponse(
     val interviews: List<InterviewQuestion>
