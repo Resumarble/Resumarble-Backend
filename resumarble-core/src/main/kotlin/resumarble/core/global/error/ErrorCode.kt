06b617cd11a3313e6247bfd3d293495a5fb92aac
waveofmymind <sjun0913@gmail.com>
2023-09-06 16:36:18 +0900

    feat(user): 유저에 대한 기본적인 커스텀 예외를 구현한다.

--- resumarble-core/src/main/kotlin/resumarble/core/global/error/ErrorCode.kt
+++ resumarble-core/src/main/kotlin/resumarble/core/global/error/ErrorCode.kt
@@ -17,5 +17,9 @@ enum class ErrorCode(
     // prompt
     PROMPT_NOT_FOUND(HttpStatus.NOT_FOUND, "프롬프트를 찾을 수 없습니다"),
     PROMPT_ALREADY_EXISTS(HttpStatus.CONFLICT, "프롬프트가 이미 존재합니다"),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 에러입니다.")
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 에러입니다."),
+
+    // user
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다"),
+    UNIDENTIFIED_USER(HttpStatus.UNAUTHORIZED, "사용자 정보가 일치하지 않습니다.")
 }
