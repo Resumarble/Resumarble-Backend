08a90dca1053837161a60dc792aa4b28e50c174d
waveofmymind <sjun0913@gmail.com>
2023-09-06 21:22:23 +0900

    test(user): Password를 사용한 회원 생성 테스트로 변경한다.

--- resumarble-api/src/test/kotlin/resumarble/core/domain/user/domain/UserTest.kt
+++ resumarble-api/src/test/kotlin/resumarble/core/domain/user/domain/UserTest.kt
@@ -2,6 +2,7 @@ package resumarble.core.domain.user.domain
 
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import resumarble.core.domain.user.domain.password.Password
 
 class UserTest : StringSpec() {
 
@@ -12,12 +13,12 @@ class UserTest : StringSpec() {
             val nickname = "nickname"
             val user = User(
                 email = email,
-                password = password,
+                password = Password(password),
                 nickname = nickname
             )
 
             user.email shouldBe email
-            user.password shouldBe password
+            user.password shouldBe Password(password)
             user.nickname shouldBe nickname
         }
     }
