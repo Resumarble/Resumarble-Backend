e518e7de4bf8d54fae5ff79992dc05e0d27ec5b4
waveofmymind <sjun0913@gmail.com>
2023-09-09 17:54:26 +0900

    feat(user): 회원 정보에 관한 클래스를 분리한다.

--- resumarble-core/src/main/kotlin/resumarble/core/domain/user/domain/User.kt
+++ resumarble-core/src/main/kotlin/resumarble/core/domain/user/domain/User.kt
@@ -1,18 +1,37 @@
 package resumarble.core.domain.user.domain
 
+import resumarble.core.domain.user.constraints.JwtProvider
+import resumarble.core.domain.user.constraints.UserRole
 import resumarble.core.domain.user.domain.password.Password
 import resumarble.core.global.error.UnidentifiedUserException
 
-class User(
+class User private constructor(
 
-    val email: String,
+    private val userInformation: UserInformation,
 
     val password: Password,
 
-    val nickname: String,
+    val provider: JwtProvider = JwtProvider.RESUMARBLE,
 
-    var userId: Long = 0L
+    val role: UserRole = UserRole.ROLE_USER,
+
+    val userId: Long = 0L
 ) {
+    constructor(
+        email: String,
+        password: Password,
+        nickname: String,
+        provider: JwtProvider,
+        role: UserRole,
+        userId: Long
+    ) : this(UserInformation(email, nickname), password, provider, role, userId)
+
+    val email: String
+        get() = userInformation.email
+
+    val nickname: String
+        get() = userInformation.nickname
+
     fun authenticate(password: Password) {
         identify(this.password == password)
     }
