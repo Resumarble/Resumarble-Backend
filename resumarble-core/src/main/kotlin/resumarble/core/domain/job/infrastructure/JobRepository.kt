5043d448e49efc7fb396daebd1b14c25cbc4ee7d
waveofmymind <sjun0913@gmail.com>
2023-09-11 19:56:16 +0900

    delete(job): Job은 백엔드에서 사용하지 않기 때문에 제거한다.

--- resumarble-core/src/main/kotlin/resumarble/core/domain/job/infrastructure/JobRepository.kt
+++
@@ -1,7 +0,0 @@
-package resumarble.core.domain.job.infrastructure
-
-import resumarble.core.domain.job.domain.Job
-
-interface JobRepository {
-    fun findAll(): List<Job>
-}
--- resumarble-infrastructure/src/main/kotlin/resumarble/infrastructure/job/entity/JobJpaRepository.kt
+++
@@ -1,6 +0,0 @@
-package resumarble.infrastructure.job.entity
-
-import org.springframework.data.jpa.repository.JpaRepository
-import resumarble.core.domain.job.domain.Job
-
-interface JobJpaRepository : JpaRepository<Job, Long>
