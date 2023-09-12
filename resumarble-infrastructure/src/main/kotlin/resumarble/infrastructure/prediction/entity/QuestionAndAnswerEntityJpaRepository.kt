09b5b3000c058c6f48f0b6c107036be2bd23e090
waveofmymind <sjun0913@gmail.com>
2023-09-13 00:55:42 +0900

    feat(qa): predictionId로 QA 테이블을 조회하는 로직을 구현한다.

--- resumarble-infrastructure/src/main/kotlin/resumarble/infrastructure/prediction/entity/QuestionAndAnswerEntityJpaRepository.kt
+++ resumarble-infrastructure/src/main/kotlin/resumarble/infrastructure/prediction/entity/QuestionAndAnswerEntityJpaRepository.kt
@@ -2,4 +2,7 @@ package resumarble.infrastructure.prediction.entity
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface QuestionAndAnswerEntityJpaRepository : JpaRepository<QuestionAndAnswerEntity, Long>
+interface QuestionAndAnswerEntityJpaRepository : JpaRepository<QuestionAndAnswerEntity, Long> {
+
+    fun findAllByPredictionId(predictionId: Long): List<QuestionAndAnswerEntity>
+}
