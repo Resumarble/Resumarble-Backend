d76c7122f53460541bf3f7d765814a1378b4587f
waveofmymind <sjun0913@gmail.com>
2023-09-16 21:25:32 +0900

    test(user): 패스워드 암호화 테스트를 구현한다.

---
+++ resumarble-api/src/test/kotlin/resumarble/core/domain/user/domain/PasswordTest.kt
@@ -0,0 +1,19 @@
+package resumarble.core.domain.user.domain
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import resumarble.core.domain.user.domain.password.sha256Encrypt
+
+class PasswordTest : StringSpec({
+    "sha256Encrypt - 동일한 입력에 동일한 결과를 출력한다" {
+        val input = "password"
+        sha256Encrypt(input) shouldBe sha256Encrypt(input)
+    }
+
+    "sha256Encrypt - 충돌하지 않는 경우를 테스트한다" {
+        val first = "1q2w3e4r!"
+        val second = "2w3e4r5t!"
+        sha256Encrypt(first) shouldNotBe sha256Encrypt(second)
+    }
+})
