eb746441a8184cd32356cf9eb5957feff9cd5f61
waveofmymind <sjun0913@gmail.com>
2023-09-17 20:38:21 +0900

    feat(prediction): PredictionFacade를 추가해서 InterviewQuestionFacade에 대한 책임을 분리한다.

--- resumarble-core/src/main/kotlin/resumarble/core/domain/resume/facade/InterviewQuestionFacade.kt
+++ resumarble-core/src/main/kotlin/resumarble/core/domain/resume/facade/InterviewQuestionFacade.kt
@@ -2,30 +2,37 @@ package resumarble.core.domain.resume.facade
 
 import resumarble.core.domain.gpt.OpenAiMapper
 import resumarble.core.domain.gpt.application.OpenAiService
+import resumarble.core.domain.prediction.facade.PredictionFacade
 import resumarble.core.domain.prompt.application.PromptService
 import resumarble.core.domain.prompt.domain.PromptType
 import resumarble.core.global.annotation.Facade
-import resumarble.core.global.util.loggingStopWatch
 
 @Facade
 class InterviewQuestionFacade(
     private val promptService: PromptService,
     private val openAiService: OpenAiService,
-    private val openAiMapper: OpenAiMapper
+    private val openAiMapper: OpenAiMapper,
+    private val predictionFacade: PredictionFacade
 ) {
 
     fun generateInterviewQuestion(command: InterviewQuestionCommand): InterviewQuestionResponse {
-        return loggingStopWatch {
-            val promptResponse = promptService.getPrompt(PromptType.INTERVIEW_QUESTION)
-            val completionRequestForm = command.toRequestForm(promptResponse, PROMPT_LANGUAGE)
+        val promptResponse = promptService.getPrompt(PromptType.INTERVIEW_QUESTION)
+        val completionRequestForm = command.toRequestForm(promptResponse, PROMPT_LANGUAGE)
 
-            val completionRequest =
-                openAiMapper.promptAndContentToChatCompletionRequest(completionRequestForm, promptResponse.content)
+        val completionRequest =
+            openAiMapper.promptAndContentToChatCompletionRequest(completionRequestForm, promptResponse.content)
 
-            openAiMapper.completionToInterviewQuestionResponse(
-                openAiService.requestChatCompletion(completionRequest)
-            )
-        }
+        val completionResult = openAiMapper.completionToInterviewQuestionResponse(
+            openAiService.requestChatCompletion(completionRequest)
+        )
+
+        predictionFacade.savePrediction(
+            command.job,
+            command.category,
+            completionResult
+        )
+
+        return completionResult
     }
 
     companion object {
