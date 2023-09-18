cecdb742ccc064d5647eec0014d33b40aa6797af
waveofmymind <sjun0913@gmail.com>
2023-09-18 21:54:41 +0900

    feat(prediction): 코루틴을 사용해서 Prediction을 저장하고, 예외 발생시 핸들링해서 로그로 남긴다.

--- resumarble-core/src/main/kotlin/resumarble/core/domain/resume/facade/InterviewQuestionFacade.kt
+++ resumarble-core/src/main/kotlin/resumarble/core/domain/resume/facade/InterviewQuestionFacade.kt
@@ -1,11 +1,21 @@
 package resumarble.core.domain.resume.facade
 
+import jakarta.annotation.PreDestroy
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import resumarble.core.domain.gpt.ChatCompletionRequest
 import resumarble.core.domain.gpt.OpenAiMapper
 import resumarble.core.domain.gpt.application.OpenAiService
 import resumarble.core.domain.prediction.facade.PredictionFacade
+import resumarble.core.domain.prompt.application.PromptResponse
 import resumarble.core.domain.prompt.application.PromptService
 import resumarble.core.domain.prompt.domain.PromptType
 import resumarble.core.global.annotation.Facade
+import resumarble.core.global.util.loggingErrorMarking
+import resumarble.core.global.util.loggingStopWatch
 
 @Facade
 class InterviewQuestionFacade(
@@ -14,28 +24,53 @@ class InterviewQuestionFacade(
     private val openAiMapper: OpenAiMapper,
     private val predictionFacade: PredictionFacade
 ) {
+    private val scope = CoroutineScope(Dispatchers.Default)
 
     fun generateInterviewQuestion(command: InterviewQuestionCommand): InterviewQuestionResponse {
-        val promptResponse = promptService.getPrompt(PromptType.INTERVIEW_QUESTION)
-        val completionRequestForm = command.toRequestForm(promptResponse, PROMPT_LANGUAGE)
+        val completionResult = loggingStopWatch {
+            val promptResponse = promptService.getPrompt(PromptType.INTERVIEW_QUESTION)
+            val completionRequest = prepareCompletionRequest(command, promptResponse)
+            requestChatCompletion(completionRequest)
+        }
 
-        val completionRequest =
-            openAiMapper.promptAndContentToChatCompletionRequest(completionRequestForm, promptResponse.content)
-
-        val completionResult = openAiMapper.completionToInterviewQuestionResponse(
-            openAiService.requestChatCompletion(completionRequest)
-        )
-
-        predictionFacade.savePrediction(
-            command.job,
-            command.category,
-            completionResult
-        )
+        scope.launch(handler) {
+            predictionFacade.savePrediction(
+                command.job,
+                command.category,
+                completionResult
+            )
+        }
 
         return completionResult
     }
 
+    private fun prepareCompletionRequest(
+        command: InterviewQuestionCommand,
+        promptResponse: PromptResponse
+    ): ChatCompletionRequest {
+        val completionRequestForm = command.toRequestForm(promptResponse, PROMPT_LANGUAGE)
+        return openAiMapper.promptAndContentToChatCompletionRequest(completionRequestForm, promptResponse.content)
+    }
+
+    private fun requestChatCompletion(completionRequest: ChatCompletionRequest): InterviewQuestionResponse {
+        return openAiMapper.completionToInterviewQuestionResponse(
+            openAiService.requestChatCompletion(completionRequest)
+        )
+    }
+
+    @PreDestroy
+    fun cleanUp() {
+        scope.cancel()
+    }
+
+    val handler = CoroutineExceptionHandler { _, throwable ->
+        loggingErrorMarking {
+            SAVE_PREDICTION_ERROR_MESSAGE + "${throwable.message}"
+        }
+    }
+
     companion object {
         private const val PROMPT_LANGUAGE = "korean"
+        private const val SAVE_PREDICTION_ERROR_MESSAGE = "면접 예상 질문 저장이 실패했습니다. 예외 메시지: "
     }
 }
