5cb6f8445e92e7cfb18d6c2f5bf6cc058a2180f9
waveofmymind <sjun0913@gmail.com>
2023-09-24 10:04:34 +0900

    feat(prediction): 직업 목록을 추가한다.

--- resumarble-core/src/main/kotlin/resumarble/core/domain/prediction/domain/constraints/Job.kt
+++ resumarble-core/src/main/kotlin/resumarble/core/domain/prediction/domain/constraints/Job.kt
@@ -2,12 +2,12 @@ package resumarble.core.domain.prediction.domain.constraints
 
 enum class Job(val jobTitleKr: String, val jobTitleEn: String) {
     BACKEND_ENGINEER("백엔드 개발자", "Backend Engineer"),
-    FRONTEND_ENGINEER("프론트엔드 개발자", "Frontend Developer"),
+    FRONTEND_ENGINEER("프론트 개발자", "Frontend Engineer"),
+    FULLSTACK_ENGINEER("풀스택 개발자", "Fullstack Engineer"),
     ANDROID_DEVELOPER("안드로이드 개발자", "Android Developer"),
     IOS_DEVELOPER("iOS 개발자", "iOS Developer"),
     DEVOPS_ENGINEER("데브옵스 엔지니어", "DevOps Engineer"),
     DATA_ENGINEER("데이터 엔지니어", "Data Engineer"),
-    DATA_SCIENTIST("데이터 사이언티스트", "Data Scientist"),
     MACHINE_LEARNING_ENGINEER("머신러닝 엔지니어", "Machine Learning Engineer"),
     PRODUCT_MANAGER("프로덕트 매니저", "Product Manager"),
     PROJECT_MANAGER("프로젝트 매니저", "Project Manager");
@@ -15,7 +15,8 @@ enum class Job(val jobTitleKr: String, val jobTitleEn: String) {
     companion object {
         @JvmStatic
         fun fromJobTitleEn(title: String): Job {
-            return values().firstOrNull { it.jobTitleEn == title } ?: throw IllegalArgumentException("Invalid job title: $title")
+            return entries.firstOrNull { it.jobTitleEn == title }
+                ?: throw IllegalArgumentException("Invalid job title: $title")
         }
     }
 }
