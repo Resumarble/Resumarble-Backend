94e6fd606621b01bcfdda7c8283b375f9b890cb1
waveofmymind <sjun0913@gmail.com>
2023-09-28 09:52:58 +0900

    fix(root): Spring Cloud Config와 resilience4j 의존성을 제거한다.

--- resumarble-api/build.gradle.kts
+++ resumarble-api/build.gradle.kts
@@ -12,11 +12,6 @@ extra["springCloudVersion"] = "2022.0.4"
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
 
-    // spring cloud config
-    implementation("org.springframework.cloud:spring-cloud-starter-config")
-
-    implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j")
-
     // swagger-ui
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
 
