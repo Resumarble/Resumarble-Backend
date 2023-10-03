0ac0a43529bbf4fcb6bd4bb1342a1cfa21fecc7e
waveofmymind <sjun0913@gmail.com>
2023-10-03 20:21:24 +0900

    feat(log): UserRequestLog를 저장하는 인터페이스 함수를 추가한다.

--- resumarble-core/src/main/kotlin/resumarble/core/domain/log/infrastructure/UserRequestLogRepository.kt
+++ resumarble-core/src/main/kotlin/resumarble/core/domain/log/infrastructure/UserRequestLogRepository.kt
@@ -1,3 +1,8 @@
 package resumarble.core.domain.log.infrastructure
 
-interface UserRequestLogRepository
+import resumarble.core.domain.log.application.UserRequestLogCommand
+
+interface UserRequestLogRepository {
+
+    fun saveUserRequestLog(command: UserRequestLogCommand)
+}
