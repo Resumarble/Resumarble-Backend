506c00fe3675057ba9c0c3a6a41a924b2c3de621
waveofmymind <sjun0913@gmail.com>
2023-10-07 02:28:51 +0900

    feat(log): 코루틴을 활용해서 유저 로그를 저장하고, 예외를 로깅하여 핸들링한다.

--- resumarble-core/src/main/kotlin/resumarble/core/domain/log/application/UserRequestLogService.kt
+++ resumarble-core/src/main/kotlin/resumarble/core/domain/log/application/UserRequestLogService.kt
@@ -1,15 +1,33 @@
 package resumarble.core.domain.log.application
 
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import resumarble.core.domain.log.infrastructure.UserRequestLogRepository
+import resumarble.core.global.util.loggingErrorMarking
 
 @Service
 @Transactional
 class UserRequestLogService(
     private val userRequestLogRepository: UserRequestLogRepository
 ) {
+    val scope = CoroutineScope(Dispatchers.IO)
     fun saveUserRequestLog(command: UserRequestLogCommand) {
-        userRequestLogRepository.saveUserRequestLog(command)
+        scope.launch(handler) {
+            userRequestLogRepository.saveUserRequestLog(command)
+        }
+    }
+
+    val handler = CoroutineExceptionHandler { _, throwable ->
+        loggingErrorMarking {
+            SAVE_USER_LOG_ERROR_MESSAGE + throwable.message
+        }
+    }
+
+    companion object {
+        private const val SAVE_USER_LOG_ERROR_MESSAGE = "유저 로그 저장에 실패했습니다."
     }
 }
