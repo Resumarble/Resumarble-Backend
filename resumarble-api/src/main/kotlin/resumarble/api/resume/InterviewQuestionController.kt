1a9955e7ed53121338f1ed82d21353fa1483c611
waveofmymind <sjun0913@gmail.com>
2023-10-11 00:00:51 +0900

    #28 feat(interview-questions): 테스트용 API를 스웨거에 추가하기 위한 핸들러 메서드를 구현한다.

--- resumarble-api/src/main/kotlin/resumarble/api/resume/InterviewQuestionController.kt
+++ resumarble-api/src/main/kotlin/resumarble/api/resume/InterviewQuestionController.kt
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import resumarble.api.global.jwt.JwtUserDetails
 import resumarble.api.global.response.Response
+import resumarble.core.domain.resume.facade.InterviewQuestion
 import resumarble.core.domain.resume.facade.InterviewQuestionFacade
 import resumarble.core.domain.resume.facade.InterviewQuestionResponse
 
@@ -28,5 +29,68 @@ class InterviewQuestionController(
     ): Response<InterviewQuestionResponse> {
         val command = request.toCommand(user?.userId ?: 0L)
         return Response.ok(interviewQuestionFacade.generateInterviewQuestion(command))
+    }
+
+    @Operation(
+        operationId = "interview-questions",
+        summary = "면접 예상 질문 생성 폼 여러개 요청",
+        description = "테스트 API로, 개발 완료시 /interview-questions로 변경 예정입니다."
+    )
+    @PostMapping("/multiple-interview-questions")
+    fun multipleInterviewQuestions(
+        @RequestBody request: MultipleInterviewQuestionRequest
+    ): Response<List<InterviewQuestionResponse>> {
+        return Response.ok(
+            listOf(
+                InterviewQuestionResponse(
+                    listOf(
+                        InterviewQuestion(
+                            "Question",
+                            "Answer"
+                        ),
+                        InterviewQuestion(
+                            "Question",
+                            "Answer"
+                        ),
+                        InterviewQuestion(
+                            "Question",
+                            "Answer"
+                        )
+                    )
+                ),
+                InterviewQuestionResponse(
+                    listOf(
+                        InterviewQuestion(
+                            "Question",
+                            "Answer"
+                        ),
+                        InterviewQuestion(
+                            "Question",
+                            "Answer"
+                        ),
+                        InterviewQuestion(
+                            "Question",
+                            "Answer"
+                        )
+                    )
+                ),
+                InterviewQuestionResponse(
+                    listOf(
+                        InterviewQuestion(
+                            "Question",
+                            "Answer"
+                        ),
+                        InterviewQuestion(
+                            "Question",
+                            "Answer"
+                        ),
+                        InterviewQuestion(
+                            "Question",
+                            "Answer"
+                        )
+                    )
+                )
+            )
+        )
     }
 }
