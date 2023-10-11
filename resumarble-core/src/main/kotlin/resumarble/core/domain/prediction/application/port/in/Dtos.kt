12eed257e42404a41fe2eb82a194bc9277f95354
waveofmymind <sjun0913@gmail.com>
2023-10-11 15:54:05 +0900

    refactor(prediction): JSON 데이터의 key 값을 카멜 케이스로 변경한다.

--- resumarble-core/src/main/kotlin/resumarble/core/domain/prediction/application/port/in/Dtos.kt
+++ resumarble-core/src/main/kotlin/resumarble/core/domain/prediction/application/port/in/Dtos.kt
@@ -1,16 +1,12 @@
 package resumarble.core.domain.prediction.application.port.`in`
 
-import com.fasterxml.jackson.annotation.JsonProperty
 import java.time.LocalDateTime
 
 data class PredictionResponse(
-    @JsonProperty("prediction_id")
     val predictionId: Long,
     val job: String,
     val category: String,
-    @JsonProperty("question_and_answer")
     val questionAndAnswer: List<QuestionAndAnswerResponse>,
-    @JsonProperty("created_date")
     val createdDate: LocalDateTime
 )
 
