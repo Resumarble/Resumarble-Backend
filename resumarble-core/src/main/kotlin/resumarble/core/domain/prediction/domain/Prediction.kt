7bf69c650110da819d576f81dfda80fe9fd57746
waveofmymind <sjun0913@gmail.com>
2023-10-11 15:55:52 +0900

    feat(prediction): Prediction 자신이 권한을 체크하는 메서드를 구현한다.

--- resumarble-core/src/main/kotlin/resumarble/core/domain/prediction/domain/Prediction.kt
+++ resumarble-core/src/main/kotlin/resumarble/core/domain/prediction/domain/Prediction.kt
@@ -2,6 +2,7 @@ package resumarble.core.domain.prediction.domain
 
 import resumarble.core.domain.prediction.domain.constraints.Category
 import resumarble.core.domain.prediction.domain.constraints.Job
+import resumarble.core.global.error.UnIdentifiedException
 import java.time.LocalDateTime
 
 class Prediction(
@@ -21,4 +22,10 @@ class Prediction(
 ) {
     val questionAndAnswer: List<QuestionAndAnswer>
         get() = questionAndAnswerList.toList()
+
+    fun authenticate(userId: Long) {
+        if (this.userId != userId) {
+            throw UnIdentifiedException()
+        }
+    }
 }
