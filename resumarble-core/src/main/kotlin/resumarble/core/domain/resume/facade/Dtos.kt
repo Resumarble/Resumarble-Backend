54cc600a914f5c0e68a086a2bfb6d96ddade89c1
waveofmymind <sjun0913@gmail.com>
2023-10-24 00:03:24 +0900

    refactor(interview-question)!: 프롬프트 언어를 상수화하지 않는다.
    추후 다국어를 지원할 때 열거형으로 관리할 것이다.

--- resumarble-core/src/main/kotlin/resumarble/core/domain/resume/facade/Dtos.kt
+++ resumarble-core/src/main/kotlin/resumarble/core/domain/resume/facade/Dtos.kt
@@ -4,12 +4,15 @@ import resumarble.core.domain.log.application.UserRequestLogCommand
 import resumarble.core.domain.log.constraints.RequestOutcome
 import resumarble.core.domain.prompt.application.PromptResponse
 
+private const val PROMPT_LANGUAGE = "korean"
+
 data class InterviewQuestionCommand(
     val userId: Long = 0L,
     val job: String,
     val career: String,
     val category: String,
-    val content: String
+    val content: String,
+    val language: String = PROMPT_LANGUAGE
 ) {
     fun toRequestForm(prompt: PromptResponse, language: String): String {
         return prompt.createRequestForm(job, category, career, language)
