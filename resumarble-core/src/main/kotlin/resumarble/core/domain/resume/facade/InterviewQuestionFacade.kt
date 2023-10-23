938ad4a5cad156f661dfbbd103ac9e74228dd2ef
waveofmymind <sjun0913@gmail.com>
2023-10-23 21:14:09 +0900

    feat(interview-question): 질문 생성 실패시 예외를 잡아 커스텀 예외를 발생시킨다.

--- resumarble-core/src/main/kotlin/resumarble/core/domain/resume/facade/InterviewQuestionFacade.kt
+++ resumarble-core/src/main/kotlin/resumarble/core/domain/resume/facade/InterviewQuestionFacade.kt
@@ -1,5 +1,6 @@
 package resumarble.core.domain.resume.facade
 
+import feign.FeignException.FeignClientException
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
@@ -7,12 +8,14 @@ import kotlinx.coroutines.coroutineScope
 import resumarble.core.domain.gpt.ChatCompletionRequest
 import resumarble.core.domain.gpt.OpenAiMapper
 import resumarble.core.domain.gpt.application.OpenAiService
-import resumarble.core.domain.log.application.UserRequestLogService
+import resumarble.core.domain.log.application.UserRequestLogWriter
 import resumarble.core.domain.prediction.facade.PredictionFacade
 import resumarble.core.domain.prompt.application.PromptResponse
 import resumarble.core.domain.prompt.application.PromptService
 import resumarble.core.domain.prompt.domain.PromptType
 import resumarble.core.global.annotation.Facade
+import resumarble.core.global.error.CompletionFailedException
+import resumarble.core.global.util.logger
 import resumarble.core.global.util.loggingStopWatch
 
 @Facade
@@ -21,7 +24,7 @@ class InterviewQuestionFacade(
     private val openAiService: OpenAiService,
     private val openAiMapper: OpenAiMapper,
     private val predictionFacade: PredictionFacade,
-    private val userRequestLogService: UserRequestLogService
+    private val userRequestLogWriter: UserRequestLogWriter
 ) {
     suspend fun generateInterviewQuestions(commands: List<InterviewQuestionCommand>): List<InterviewQuestion> {
         return coroutineScope {
@@ -40,9 +43,9 @@ class InterviewQuestionFacade(
         val completionRequest = prepareCompletionRequest(command, promptResponse)
 
         val completionResult =
-            loggingStopWatch { requestChatCompletion(completionRequest) }
+            loggingStopWatch { requestChatCompletion(completionRequest, command.userId, command.content) }
 
-        userRequestLogService.saveUserRequestLog(command.toSaveLogCommand())
+        userRequestLogWriter.saveUserRequestLog(command.toSaveLogCommand())
 
         predictionFacade.savePrediction(openAiMapper.completionToSavePredictionCommand(command, completionResult))
 
@@ -57,10 +60,23 @@ class InterviewQuestionFacade(
         return openAiMapper.promptAndContentToChatCompletionRequest(completionRequestForm, command.content)
     }
 
-    private fun requestChatCompletion(completionRequest: ChatCompletionRequest): List<InterviewQuestion> {
-        return openAiMapper.completionToInterviewQuestionResponse(
-            openAiService.requestChatCompletion(completionRequest)
-        )
+    private fun requestChatCompletion(
+        completionRequest: ChatCompletionRequest,
+        userId: Long,
+        userContent: String
+    ): List<InterviewQuestion> {
+        try {
+            val completionResult = openAiService.requestChatCompletion(completionRequest)
+            return openAiMapper.completionToInterviewQuestionResponse(
+                completionResult
+            )
+        } catch (e: FeignClientException) {
+            logger.error(e.message, e)
+            throw CompletionFailedException(
+                userId = userId,
+                userContent = userContent
+            )
+        }
     }
 
     companion object {
