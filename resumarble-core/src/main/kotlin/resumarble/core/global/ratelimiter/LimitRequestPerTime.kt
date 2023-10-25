2eeed224793214a39e6936a4b466acffb608dc29
waveofmymind <sjun0913@gmail.com>
2023-10-25 23:23:04 +0900

    feat(rate-limit): AOP로 사용할 어노테이션을 구현한다.

---
+++ resumarble-core/src/main/kotlin/resumarble/core/global/ratelimiter/LimitRequestPerTime.kt
@@ -0,0 +1,27 @@
+package resumarble.core.global.ratelimiter
+
+import java.util.concurrent.TimeUnit
+
+@Target(AnnotationTarget.TYPE, AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class LimitRequestPerTime(
+    /**
+     * 분당호출 제한시킬 unique key prefix
+     */
+    val prefix: String = "",
+
+    /**
+     * 호출 제한 시간
+     */
+    val ttl: Int = 1,
+
+    /**
+     * 호출 제한 시간 단위
+     */
+    val ttlTimeUnit: TimeUnit = TimeUnit.SECONDS,
+
+    /**
+     * 분당 호출제한 카운트
+     */
+    val count: Int = 1
+)
