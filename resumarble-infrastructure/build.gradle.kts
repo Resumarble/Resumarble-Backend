9fd25346806882db54c7ee3141443e45626b1bfb
waveofmymind <sjun0913@gmail.com>
2023-10-30 00:16:59 +0900

    feat(root): spring-data-kotlin-jdsl 의존성을 추가한다.

--- resumarble-infrastructure/build.gradle.kts
+++ resumarble-infrastructure/build.gradle.kts
@@ -10,6 +10,7 @@ dependencies {
 
     implementation("com.linecorp.kotlin-jdsl:jpql-dsl:3.0.0")
     implementation("com.linecorp.kotlin-jdsl:jpql-render:3.0.0")
+    implementation("com.linecorp.kotlin-jdsl:spring-data-jpa-support:3.0.0")
 
     runtimeOnly("com.mysql:mysql-connector-j")
 
