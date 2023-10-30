cb02c16864aac8d51cba1743909eed188fd420c6
waveofmymind <sjun0913@gmail.com>
2023-10-30 23:16:49 +0900

    fix(prediction): supervisorJob을 이용해서 자식 코루틴의 예외 전파 문제를 해결한다.

--- resumarble-core/src/main/kotlin/resumarble/core/domain/prediction/facade/PredictionFacade.kt
+++ resumarble-core/src/main/kotlin/resumarble/core/domain/prediction/facade/PredictionFacade.kt
@@ -1,10 +1,9 @@
 package resumarble.core.domain.prediction.facade
 
-import jakarta.annotation.PreDestroy
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.cancel
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 import resumarble.core.domain.prediction.application.port.`in`.SavePredictionUseCase
 import resumarble.core.global.annotation.Facade
@@ -14,10 +13,8 @@ import resumarble.core.global.util.loggingErrorMarking
 class PredictionFacade(
     private val savePredictionUseCase: SavePredictionUseCase
 ) {
-    private val scope = CoroutineScope(Dispatchers.IO)
-
     fun savePrediction(command: SavePredictionCommand) {
-        scope.launch(handler) {
+        CoroutineScope(Dispatchers.IO + handler).launch(SupervisorJob()) {
             savePredictionUseCase.savePrediction(command.toDomain())
         }
     }
@@ -26,11 +23,6 @@ class PredictionFacade(
         loggingErrorMarking {
             SAVE_PREDICTION_ERROR_MESSAGE + throwable.message
         }
-    }
-
-    @PreDestroy
-    fun cleanUp() {
-        scope.cancel()
     }
 
     companion object {
