ccc30474c80341d0ff3eab20afd412a4f8a314b5
waveofmymind <sjun0913@gmail.com>
2023-11-24 20:45:31 +0900

    feat(reactor)!: reactor 모듈을 추가한다.
    별도의 부트 서버가 뜰 것이기 때문에 BootJar = True이다.

---
+++ resumarble-reactor/build.gradle.kts
@@ -0,0 +1,23 @@
+tasks.getByName<Jar>("bootJar") {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    enabled = true
+}
+
+tasks.getByName<Jar>("jar") {
+    enabled = false
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+    developmentOnly("io.netty:netty-resolver-dns-native-macos:4.1.75.Final") {
+        artifact { classifier = "osx-aarch_64" }
+    }
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+}
