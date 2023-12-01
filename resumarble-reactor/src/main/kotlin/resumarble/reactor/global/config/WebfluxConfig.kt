ec64204d91f04de0c96154635a3efc568473be5f
waveofmymind <sjun0913@gmail.com>
2023-12-01 17:00:51 +0900

    feat(reactor): @EnableWebFlux 클래스를 구현한다.

---
+++ resumarble-reactor/src/main/kotlin/resumarble/reactor/global/config/WebfluxConfig.kt
@@ -0,0 +1,8 @@
+package resumarble.reactor.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.config.EnableWebFlux
+
+@Configuration
+@EnableWebFlux
+class WebfluxConfig
