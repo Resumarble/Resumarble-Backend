09aa0344d4304953bd1dde9c0e8c856d61fcbe5c
waveofmymind <sjun0913@gmail.com>
2023-12-01 17:45:12 +0900

    style(gateway): 폴더 구조를 변경한다.

--- resumarble-gateway/src/main/kotlin/resumarble/gateway/config/WebFluxConfig.kt
+++ resumarble-gateway/src/main/kotlin/resumarble/gateway/config/WebFluxConfig.kt
@@ -1,4 +1,4 @@
-package resumarble.config
+package resumarble.gateway.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.config.EnableWebFlux
