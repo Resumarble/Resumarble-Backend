5ebb90ebadf30405a8382f095e8d4acea0b8d1fb
waveofmymind <sjun0913@gmail.com>
2023-12-01 17:28:01 +0900

    feat(gateway): 게이트웨이 모듈을 추가하고, 기본 설정을 구현한다.

---
+++ resumarble-gateway/build.gradle.kts
@@ -0,0 +1,24 @@
+tasks.getByName<Jar>("bootJar") {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    enabled = true
+}
+
+tasks.getByName<Jar>("jar") {
+    enabled = false
+}
+
+extra["springCloudVersion"] = "2022.0.4"
+
+dependencies {
+
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    developmentOnly("io.netty:netty-resolver-dns-native-macos:4.1.75.Final") {
+        artifact { classifier = "osx-aarch_64" }
+    }
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
