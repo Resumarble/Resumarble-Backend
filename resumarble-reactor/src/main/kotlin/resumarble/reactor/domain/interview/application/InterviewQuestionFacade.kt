178cb184cd82780cfdef1e920aeedcbc6fe88551
waveofmymind <sjun0913@gmail.com>
2023-12-04 16:18:20 +0900

    fix(interview-question)!: 코루틴 실행 결과 순서에 따른 예외 규칙을 수정한다.
    1. chatCompletionReader가 실패할 경우 전체 로직이 실패해야한다.
    2. InterviewQuestionWriter가 실패할 경우에는 메인 로직에 영향을 주지 않는다.(성공한다.

--- resumarble-reactor/src/main/kotlin/resumarble/reactor/domain/interview/application/InterviewQuestionFacade.kt
+++ resumarble-reactor/src/main/kotlin/resumarble/reactor/domain/interview/application/InterviewQuestionFacade.kt
@@ -1,14 +1,17 @@
 package resumarble.reactor.domain.interview.application
 
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.supervisorScope
+import org.slf4j.LoggerFactory
 import resumarble.reactor.domain.gpt.application.ChatCompletionReader
 import resumarble.reactor.domain.interview.domain.Category
 import resumarble.reactor.global.annotation.Facade
+import java.time.LocalDateTime
 
 @Facade
 class InterviewQuestionFacade(
@@ -33,15 +36,23 @@ class InterviewQuestionFacade(
                 chatCompletionReader.readChatCompletion(command)
             }
 
-            val interviewQuestionCommands = completionResult.await().map {
-                it.toCommand(command.userId, Category.fromValue(command.category))
-            }
-
-            launch(Dispatchers.IO) {
-                interviewQuestionWriter.save(interviewQuestionCommands)
+            supervisorScope {
+                launch(Dispatchers.IO + handler) {
+                    val predictionResponses = completionResult.await().map {
+                        it.toCommand(command.userId, Category.fromValue(command.category))
+                    }
+                    interviewQuestionWriter.save(predictionResponses)
+                }
             }
 
             completionResult.await()
         }
     }
+
+    private val handler = CoroutineExceptionHandler { _, exception ->
+        log.error("사용자 결과를 저장하는데 예외가 발생했습니다. $exception")
+        log.error("예외 발생 시각: " + LocalDateTime.now())
+    }
+
+    private val log = LoggerFactory.getLogger(this::class.java)
 }
