98e3392cb070e84814b295ef2c004e7991f4e058
waveofmymind <sjun0913@gmail.com>
2023-12-11 02:14:37 +0900

    feat(interview-questions): 면접 예상 질문을 조회하는 핸들링 함수를 구현한다.

--- resumarble-reactor/src/main/kotlin/resumarble/reactor/domain/interview/controller/InterviewQuestionApi.kt
+++ resumarble-reactor/src/main/kotlin/resumarble/reactor/domain/interview/controller/InterviewQuestionApi.kt
@@ -1,9 +1,12 @@
 package resumarble.reactor.domain.interview.controller
 
+import org.springframework.data.domain.PageRequest
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import resumarble.reactor.domain.interview.application.InterviewQuestionFacade
 
@@ -18,6 +21,16 @@ class InterviewQuestionApi(
         @RequestBody request: InterviewQuestionRequest,
         @RequestHeader(X_AUTHORIZATION_ID, defaultValue = "0") userId: String
     ) = interviewQuestionFacade.generateInterviewQuestions(request.toCommandList(userId.toLong()))
+
+    @GetMapping
+    fun readInterviewQuestion(
+        @RequestParam(
+            defaultValue = "0"
+        ) page: Int,
+        @RequestHeader(X_AUTHORIZATION_ID) userId: String
+    ) {
+        interviewQuestionFacade.readInterviewQuestions(userId.toLong(), PageRequest.of(page, 10))
+    }
 
     companion object {
         private const val X_AUTHORIZATION_ID = "X-Authorization-Id"
