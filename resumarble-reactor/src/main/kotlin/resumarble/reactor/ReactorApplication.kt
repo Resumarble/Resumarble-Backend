f303c1c28b24fc3c23704674c0bf8ab26ce60bea
waveofmymind <sjun0913@gmail.com>
2023-12-13 22:16:11 +0900

    feat(root): blockHound를 부트 런 전에 수행한다.

--- resumarble-reactor/src/main/kotlin/resumarble/reactor/ReactorApplication.kt
+++ resumarble-reactor/src/main/kotlin/resumarble/reactor/ReactorApplication.kt
@@ -3,11 +3,13 @@ package resumarble.reactor
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import reactor.blockhound.BlockHound
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
 class ReactorApplication
 
 fun main(args: Array<String>) {
+    BlockHound.install()
     runApplication<ReactorApplication>(*args)
 }
