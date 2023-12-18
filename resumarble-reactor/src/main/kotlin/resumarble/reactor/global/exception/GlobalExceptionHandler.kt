03f40e63f210bae6413a31e6491dea398389e97c
waveofmymind <sjun0913@gmail.com>
2023-12-18 18:35:52 +0900

    refactor(reactor): Mono를 반환해서 논블로킹하게 예외를 핸들링한다.

--- resumarble-reactor/src/main/kotlin/resumarble/reactor/global/exception/GlobalExceptionHandler.kt
+++ resumarble-reactor/src/main/kotlin/resumarble/reactor/global/exception/GlobalExceptionHandler.kt
@@ -3,15 +3,19 @@ package resumarble.reactor.global.exception
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import reactor.core.publisher.Mono
 
 @ControllerAdvice
 class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException::class)
-    fun handleBusinessException(e: BusinessException): ResponseEntity<ErrorResponse> {
-        return ResponseEntity
-            .status(e.errorCode.httpStatus)
-            .body(ErrorResponse(e.errorCode.name, e.errorCode.message))
+    fun handleBusinessException(e: BusinessException): Mono<ResponseEntity<ErrorResponse>> {
+        val errorResponse = ErrorResponse(e.errorCode.name, e.errorCode.message)
+        return Mono.just(
+            ResponseEntity
+                .status(e.errorCode.httpStatus)
+                .body(errorResponse)
+        )
     }
 }
 
